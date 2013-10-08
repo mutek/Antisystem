bb0cbca23ab543ff8f56b2c9d83b9848b307e879
mutek <mutek@inventati.org>
2013-10-08 17:04:02 +0000

    017127419

--- compile.sh
+++ compile.sh
@@ -9,7 +9,8 @@
 # Do What The Fuck You Want License
 # 0. Just Do What The Fuck You Want
 
-
+EPOCA_UNIX="$(date +%s)"
+ATOM_MAIN_UPDATED="$(date +%Y-%M-%dT%H:%M:%SZ)"
 
 # Legge un eventuale file di configurazione dove è possibile configurare variabili che verranno usate qua e la
 [ -f ./CONFIG ] && . ./CONFIG
@@ -38,6 +39,17 @@ printf .
 # Sorgente HTML della sezione BODY la parte di testa 
 cat model/body.header.source >> index.html
 printf .
+
+# ATOM
+# Sorgente Atom header del file
+cat << ATOM_HEADER > atom.xml
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+ATOM_HEADER
+
+. ./model/atom_header.source
+
 # Riempimento automatico dei post testuali nel BODY 
 for post in $(ls -c source)
 do
@@ -53,21 +65,43 @@ cat << POST >> index.html
 POST
 
 	# un po di magia
-cat source/$post | sed  ':a;N;$!ba;s/\n/\<\/br\>/g' > $post.tmp
-sed -i 's/ at / at /g' $post.tmp
-sed -i 's/[[:cntrl:]]/ /g' $post.tmp
-sed -i 's/^[[:space:]]*$//g' $post.tmp 
-sed -i '/^$/{'"$NL"'N'"$NL"'/^\n$/D'"$NL"'}' $post.tmp
-sed -i 's/^$/<\/ul><p>/g' $post.tmp
-sed -i '/<p>$/{'"$NL"'N'"$NL"'s/\n//'"$NL"'}' $post.tmp
-sed -i 's/<p>[[:space:]]*"/<p><ul>"/' $post.tmp
-sed -i 's/^[[:space:]]*-/<br> -/g' $post.tmp
-sed -i 's/http:\/\/[[:graph:]\.\/]*/<a href="&">&<\/a> /g' $post.tmp
+	cat source/$post | sed  ':a;N;$!ba;s/\n/\<\/br\>/g' > $post.tmp
+	sed -i 's/ at / at /g' $post.tmp
+	sed -i 's/[[:cntrl:]]/ /g' $post.tmp
+	sed -i 's/^[[:space:]]*$//g' $post.tmp 
+	sed -i '/^$/{'"$NL"'N'"$NL"'/^\n$/D'"$NL"'}' $post.tmp
+	sed -i 's/^$/<\/ul><p>/g' $post.tmp
+	sed -i '/<p>$/{'"$NL"'N'"$NL"'s/\n//'"$NL"'}' $post.tmp
+	sed -i 's/<p>[[:space:]]*"/<p><ul>"/' $post.tmp
+	sed -i 's/^[[:space:]]*-/<br> -/g' $post.tmp
+	sed -i 's/http:\/\/[[:graph:]\.\/]*/<a href="&">&<\/a> /g' $post.tmp
 
 	cat $post.tmp >> index.html
 	echo "	</p>" >> index.html
-	unlink  $post.tmp
+
 	printf .
+
+##########
+# ATOM entry
+##########
+ATOM_ENTRY_UPDATED="$(stat source/$post | grep Change | cut -d"." -f 1 | cut -d " " -f 2,3 | tr ' ' 'T')Z"
+ATOM_ENTRY_SUMMARY="$(head -n 5 source/$post)"
+cat << ATOM_ENTRY >> atom.xml
+<entry>
+    <title>$TITOLO</title>
+    <link href="$Url/#$TITOLO"/>
+    <!-- id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id -->
+    <updated>$ATOM_ENTRY_UPDATED</updated>
+    <summary>$ATOM_ENTRY_SUMMARY</summary>
+    <content>
+
+ATOM_ENTRY
+
+cat $post.tmp >> atom.xml
+echo "</content></entry>" >> atom.xml
+	printf .
+	unlink  $post.tmp
+
 done
 
 # Sorgente HTML del BODY la parte di coda 
@@ -77,6 +111,9 @@ printf .
 # Tag standard universali di chiusura documento
 echo "</body>" >> index.html
 echo "</html>" >> index.html
+
+cat model/atom_footer.source >> atom.xml
+
 printf .
 echo ""
 
